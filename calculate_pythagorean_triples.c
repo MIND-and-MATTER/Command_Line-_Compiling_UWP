16337b3051edb9f4163de9709add2873abdf0030
lucca001 <>
2021-05-04 20:20:28 +0000

    Command - line

--- calculate_pythagorean_triples.c
+++ calculate_pythagorean_triples.c
@@ -2,6 +2,8 @@
 # include <math.h>
 # include <stdlib.h>
 
+double hypotenuse( int a, int b);
+
 int main(int argc, char* argv[])
 {
     int a, b, maximum_c;
@@ -16,7 +18,7 @@ int main(int argc, char* argv[])
 
     for (a = 1; a < maximum_c; a++) {
         for (b = a; b < maximum_c; b++) {
-            c = sqrt(pow((double)a, 2) + pow((double)b, 2));
+            c = hypotenuse(a, b);
             if ( (c <= (double)(maximum_c) ) &&
                  (c - floor(c) < epsilon ) ) {
                printf("%4d%4d%4d\n", a, b, (int)c);
